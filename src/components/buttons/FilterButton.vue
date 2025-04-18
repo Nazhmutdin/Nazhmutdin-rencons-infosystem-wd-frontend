485050d12554f16ac2d539456825ea4ab9cd43c9
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2025-04-18 11:00:52 +0300

    feat: implement search and filters button

---
+++ src/components/buttons/FilterButton.vue
@@ -0,0 +1,9 @@
+<template>
+    <Button icon="pi pi-filter"></Button>
+</template>
+
+<script setup lang="ts">
+    import { Button } from 'primevue';
+</script>
+
+<style scoped></style>
---
+++ src/components/buttons/SearchButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <Button icon="pi pi-search" @click="(event) => emits('click', event)"></Button>
+</template>
+
+<script setup lang="ts">
+    import { Button } from 'primevue';
+
+    const emits = defineEmits<{
+        click: [MouseEvent]
+    }>()
+</script>
+
+<style scoped></style>
