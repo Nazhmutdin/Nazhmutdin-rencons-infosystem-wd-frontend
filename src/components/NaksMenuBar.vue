9505bfcb6300bb4f2cf670ff6a472064a011028d
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2025-04-21 11:01:54 +0300

    feat: implement naks bar menu and wrap

---
+++ src/components/NaksMenuBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <Menubar :model="items">
+        <template #item="{ item, props }">
+            <router-link v-if="item.route" v-slot="{ href, navigate }" :to="{ name: item.route }" custom>
+                <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                    <span>{{ item.label }}</span>
+                </a>
+            </router-link>
+        </template>
+    </Menubar>
+</template>
+
+<script setup lang="ts">
+    import { Menubar } from 'primevue';
+    import type { MenuItem } from 'primevue/menuitem';
+
+    const items: MenuItem[] = [
+        { 
+            label: 'acst',
+            route: 'acstRegistry'
+        },
+        { 
+            label: 'acso',
+            route: 'acsoRegistry'
+        },
+        { 
+            label: 'naks builder',
+            route: 'naksBuilder'
+        }
+    ]
+</script>
+
+<style scoped></style>
---
+++ src/components/NaksMenuBarWrap.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="menu-bar-wrap">
+        <NaksMenuBar></NaksMenuBar>
+        <div class="page-content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import NaksMenuBar from './NaksMenuBar.vue';
+</script>
+
+<style scoped>
+    .p-menubar {
+        width: 50vw;
+        margin-bottom: 1vh;
+    }
+</style>
