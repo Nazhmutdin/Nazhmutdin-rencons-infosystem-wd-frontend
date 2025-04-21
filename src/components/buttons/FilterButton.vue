a7fa1f0b6ffd470bde0464bcdb75ae8f5ba2422b
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2025-04-21 11:01:07 +0300

    feat: implement filter button

--- src/components/buttons/FilterButton.vue
+++ src/components/buttons/FilterButton.vue
@@ -1,9 +1,13 @@
 <template>
-    <Button icon="pi pi-filter"></Button>
+    <Button icon="pi pi-filter" @click="(event) => emits('click', event)"></Button>
 </template>
 
 <script setup lang="ts">
     import { Button } from 'primevue';
+
+    const emits = defineEmits<{
+        click: [MouseEvent]
+    }>()
 </script>
 
 <style scoped></style>
