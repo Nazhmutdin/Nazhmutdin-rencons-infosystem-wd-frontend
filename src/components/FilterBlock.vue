af2948ec10e11ee2bd2d5820f87a8e20201f1576
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2025-04-21 11:09:21 +0300

    feat: implement filter block

---
+++ src/components/FilterBlock.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="filter-block">
+        <div class="header">
+            <slot name="header"></slot>
+        </div>
+        <div class="body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+    .header {
+        color: var(--p-text-color);
+        text-align: center;
+    }
+    .body {
+        display: flex;
+        flex-direction: column;
+        gap: 1vh;
+    }
+</style>
